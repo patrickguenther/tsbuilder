0f3983d60266cc334e89c14054d1f2816bf4a3b9
Patrick Günther <guenther@e-conomix.at>
2017-01-10 21:50:40 +0100

    bandaid for multiple tasks

--- index.js
+++ index.js
@@ -3,65 +3,57 @@ var source = require("vinyl-source-stream");
 var tsify = require("tsify");
 var gutil = require("gulp-util");
 var watchify = require("watchify");
+var gulp = require("gulp");
 
 module.exports = PgBuild;
 
-function PgBuild(gulp, files) {
+function PgBuild(name, files) {
 	if(!(this instanceof PgBuild)) {
 		console.log("Calling constructor");
-		return new PgBuild(gulp, files);
+		return new PgBuild(name, files);
 	}
 	else {
 		console.log("Is constructor");
 	}
 	
-	this.taskNames = [];
-	this.bundlers = [];
-	
-	for(var name in files) {
-		var dep = files[name];
-		var depFiles;
-		if(typeof(dep) === 'string') {
-			depFiles = [dep];
-		}
-		else {
-			depFiles = dep;
-		}
-		
-		var depFilesCleaned = [];
-		for(var i=0, f; f = depFiles[i]; ++i) {
-			depFilesCleaned.push("src/" + f + ".ts");
-		}
-		
-		console.log(depFilesCleaned);
-		
-		var w = watchify(browserify({
-			baseDir: '.',
-			debug: true,
-			entries: depFilesCleaned,
-			cache: {},
-			packageCache: {}
-		}).plugin(tsify).on('error', function(e) {console.error(e);}));
-		
-		var bundler = function() {
-			return w.bundle()
-					.pipe(source(name + '.js'))
-					.pipe(gulp.dest('dist/'))
-					.on('error', function(e) {console.error(e);})
-			;
-		};
-		
-		var taskName = 'tsbuilder-' + name;
-		gulp.task(taskName, bundler);
-		this.taskNames.push(taskName);
-		
-		this.bundlers.push(bundler);
-		
-		w.on("update", bundler);
-		w.on("log", gutil.log);
+	var depFiles;
+	if(typeof(files) === 'string') {
+		depFiles = [files];
 	}
+	else {
+		depFiles = files;
+	}
+
+	var depFilesCleaned = [];
+	for(var i=0, f; f = depFiles[i]; ++i) {
+		depFilesCleaned.push("src/" + f + ".ts");
+	}
+
+	console.log(depFilesCleaned);
+
+	var w = watchify(browserify({
+		baseDir: '.',
+		debug: true,
+		entries: depFilesCleaned,
+		cache: {},
+		packageCache: {}
+	}).plugin(tsify).on('error', function(e) {console.error(e);}));
+
+	this.bundler = function() {
+		return w.bundle()
+			.pipe(source(name + '.js'))
+			.pipe(gulp.dest('dist/'))
+			.on('error', function(e) {console.error(e);})
+		;
+	};
+
+	w.on("update", this.bundler);
+	w.on("log", gutil.log);
 }
 
-PgBuild.prototype.getTaskNames = function() {
-	return this.taskNames;
+PgBuild.prototype.getTask = function() {
+	var self = this;
+	return function() {
+		self.bundler();
+	};
 };
